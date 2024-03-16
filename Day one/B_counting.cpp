f92232f2635ba604047e68c55532e7157d9c247a
Md. Obaidullah  Al Mahdy <83227885+MDOBAIDULLAHalmahdy@users.noreply.github.com>
2024-03-16 23:58:59 +0600

    Add files via upload
    week one day one

---
+++ Day one/B_counting.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    if(b>a)
+    {
+        int c=(b-a)+1;
+        cout<<c; 
+        
+    }
+    else
+        cout<<0;
+    
+    return 0;
+}
